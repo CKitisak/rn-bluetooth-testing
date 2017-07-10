2b77772787f707a56af508fbc660d3946af7e12d
Kitisak Chaengsiri <chaengsiri.k@gmail.com>
2017-07-11 00:11:45 +0700

    setup react-native-ble-plx

--- src/index.js
+++ src/index.js
@@ -1,7 +1,42 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, TouchableOpacity, View } from 'react-native'
+
+import { BleManager } from 'react-native-ble-plx'
 
 class rnBluetoothTest extends Component {
+  componentWillMount() {
+    this.manager = new BleManager()
+
+  }
+
+  componentDidMount() {
+    const subscription = this.manager.onStateChange((state) => {
+      if (state === 'PoweredOn') {
+
+      } else if (state === 'PoweredOff') {
+
+      } else {
+
+      }
+    }, true)
+  }
+
+  _scanStart() {
+    this.manager.startDeviceScan(null, null, (error, device) => {
+      if (error) {
+        // Handle error (scanning will be stopped automatically)
+        return
+      }
+
+      // do something with found device
+    });
+  }
+
+  _scanStop () {
+    // Stop scanning as it's not necessary if you are scanning for one device.
+    this.manager.stopDeviceScan()
+  }
+
   render() {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
@@ -9,8 +44,18 @@ class rnBluetoothTest extends Component {
           Bluetooth Testing
         </Text>
         <Text>
-          with react-native-bluetooth-cross-platform
+          with react-native-ble-plx
         </Text>
+        <TouchableOpacity onPress={ this._scanStart.bind(this) }>
+          <Text>
+            Scan
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={ this._scanStop.bind(this) }>
+          <Text>
+            Stop Scanning
+          </Text>
+        </TouchableOpacity>
       </View>
     )
   }
